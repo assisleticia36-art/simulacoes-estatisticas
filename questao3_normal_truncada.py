7c9b2c03edb0aa942f15e22bd6886f848b6a6577
assisleticia36-art <assisleticia36@gmail.com>
2025-12-18 22:25:50 -0300

    Update questao3_normal_truncada.py

--- questao3_normal_truncada.py
+++ questao3_normal_truncada.py
@@ -1,1 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+media = 5000
+desvio = 1000
+min_val = 2000
+max_val = 8000
+
+amostras = []
+
+while len(amostras) < 10000:
+    valor = np.random.normal(media, desvio)
+    if min_val <= valor <= max_val:
+        amostras.append(valor)
+
+amostras = np.array(amostras)
+
+print("Média:", np.mean(amostras))
+print("Mediana:", np.median(amostras))
+print("Variância:", np.var(amostras))
+
+plt.hist(amostras, bins=30)
+plt.title("Distribuição Normal Truncada")
+plt.xlabel("Valor")
+plt.ylabel("Frequência")
+plt.show()
